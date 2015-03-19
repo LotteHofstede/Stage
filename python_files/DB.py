015d84c11646117f202855b4ffbac64487317cdc
LotteHofstede <lotte_hofstede@hotmail.com>
2015-03-19 17:27:32 +0100

    working python scripts

--- python_files/DB.py
+++ python_files/DB.py
@@ -1,58 +1,59 @@
-#!/usr/bin/python
-
-import pymongo
+from pymongo import MongoClient
 import sys
 
-class MongoDB: 
-    m, db, c;
+class MongoDB(object): 
+	m = None
+	db = None
+	c = None
   
-    def __init__ (self, inputIP, inputDBName):
-        try:
-          self.m = MongoClient(inputIP, 27017)
-          self.db = self.m.[inputDBName]
-          print "**** Connection Successful"
-        except BaseException as ex:
-          print "**** Connection Failed"
-          sys.exit()
-    
-    def set_collection(self, inputColName):
-        try:
-          self.c = self.db[inputColName]
-        except BaseException as ex:
-          print "**** Connection Failed"
-          print "**** Exit"
-          sys.exit();
+	def __init__ (self, inputIP, inputDBName):
+		try:
+		  self.m = MongoClient(inputIP, 27017)
+		  self.db = self.m[inputDBName]
+		  print "**** Connection Successful"
+		except BaseException as ex:
+		  print "**** Connection Failed"
+		  print ex
+		  sys.exit()
+
+	def set_collection(self, inputColName):
+		try:
+		  self.c = self.db[inputColName]
+		except BaseException as ex:
+		  print "**** Connection Failed"
+		  print "**** Exit"
+		  sys.exit();
 
 
-    def get_one(self, inputQuery):
-        return self.c.find_one(inputQuery)
-  
-    def get_data(self, inputQuery):
-        return self.c.find(inputQuery);
+	def get_one(self, inputQuery):
+		return self.c.find_one(inputQuery)
 
-    def get_data(self, inputQuery, inputLimit):
-        return self.c.find(inputQuery).limit(inputLimit);
-  
-    def get_data_sorted(self, inputQuery, inputSortField, inputLimit, inSortSeq):
-        return self.c.find(inputQuery).sort({inputSortField : inSortSeq}).limit(inputLimit)
+	def get_data(self, inputQuery):
+		return self.c.find(inputQuery);
 
-    def get_distinct(self, inputField):
-        return self.c.distinct(inputField)
+	def get_data(self, inputQuery, inputLimit):
+		return self.c.find(inputQuery).limit(inputLimit);
+
+	def get_data_sorted(self, inputQuery, inputSortField, inputLimit, inSortSeq):
+		return self.c.find(inputQuery).sort(inputSortField, inSortSeq).limit(inputLimit)
+
+	def get_distinct(self, inputField):
+		return self.c.distinct(inputField)
 
 
-    def get_distinct(self, inputField, inputQuery):
-        return self.c.distinct(inputField, inputQuery) 
-      
-    def get_count(self):
-        return self.c.count()
-      
-    def get count(self, inputQuery):
-        return self.c.count(inputQuery)
-      
-    def update(self, inputQuery, inputField, inputVal):
-        self.c.update(inputQuery, 
-          {"$set" :
-            {inputField : inputVal}
-            }
-        )
+	def get_distinct(self, inputField, inputQuery):
+		return self.c.distinct(inputField, inputQuery) 
+
+	def get_count(self):
+		return self.c.count()
+
+	def get_count(self, inputQuery):
+		return self.c.count(inputQuery)
+
+	def update(self, inputQuery, inputField, inputVal):
+		self.c.update(inputQuery, 
+		  {"$set" :
+		    {inputField : inputVal}
+		    }
+		)
 
--- python_files/getInstagramPicStoreDB.py
+++ python_files/getInstagramPicStoreDB.py
@@ -1,21 +1,28 @@
+#!/usr/bin/python
+
+
 import gridfs
-import DB
+from DB import MongoDB
 import mimetypes
 import requests
+import sys
 
 limit = 1000
-ipAddress = "127.0.0.1"
+ipAddress = "134.58.106.9"
 db = "images"
-people = "isGeo"
+collection = "instagram"
 
-fs = gridfs.GridFS(db)
   
-m = new MongoDB(ipAddress, db)
-m.setCollection(people)
+m = MongoDB(ipAddress, db)
+m.m.admin.authenticate('lotte', 'intern3');
+m.set_collection(collection)
 
-gfsPhoto = gridfs.GridFS(m.m, m.db)
+thisDB = MongoDB("127.0.0.1", "images"); 
+m.set_collection(collection)
 
-isExists = {"$exists": false}
+gfsPhoto = gridfs.GridFS(thisDB.db, "instagram");
+
+isExists = {"$exists" : False}
 query = {"file" : isExists}
 
 searchCursor = m.get_data_sorted(query,"created_at",limit,-1)
@@ -24,13 +31,12 @@ for curObj in searchCursor:
     _id = curObj["_id"]
     url = curObj["url"]  
     if(url.endswith(".jpg")):
-    mime_type = mimetypes.guess_type(url)[0]        
-    r = requests.get(url, stream=True)
-    _id = gfsPhoto.put(r.raw, contentType=mime_type, filename=_id)
-    print "created new gridfs file {0} with id {1}".format(gridfs_filename, _id)
-    queryEntry = {"_id":_id}
-    m.update(queryEntry,"file",true)
+	    mime_type = mimetypes.guess_type(url)[0]        
+	    r = requests.get(url, stream=True)
+	    _id = gfsPhoto.put(r.raw, contentType=mime_type, filename=_id)
+	    print "created new gridfs file from {0} with id {1}".format(url, _id)
+	    queryEntry = {"_id":_id}
+	    #m.update(queryEntry,"file",True) <I cant do this now>
 
-  
-  print "Complete"
-  exit()
+print "Complete"
+sys.exit()
