ef69ad22232f74658a6463d2f1f2c301b45f8c22
LotteHofstede <lotte_hofstede@hotmail.com>
2015-03-20 16:26:51 +0100

    php updated

--- php scripts/count_to_json.php
+++ php scripts/count_to_json.php
@@ -6,7 +6,9 @@
 	$start = new MongoDate(strtotime(date("Y-m-d 00:00:00", $time)));
 	$end = new MongoDate(strtotime(date("Y-m-d 23:59:59", $time)));
 	$cursor = $collection->find(array("date" => array('$gte' => $start, '$lte' => $end)));
+	echo '"twittercounts":[';
 	foreach($cursor as $key => $value) {
-		echo json_encode($value);
+		echo '{ "time" : "' . date('Y-m-d h:i:s', $value["date"]->sec) . '", "count" : "' . $value["count"] . '"},';
 	}
+	echo ']';
 ?>
