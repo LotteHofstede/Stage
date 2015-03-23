33ca9bef1776f53921b20a6b5e1a73e53bd03891
LotteHofstede <lotte_hofstede@hotmail.com>
2015-03-23 11:48:30 +0100

    json shell script updatet to Alvins db

--- shell scripts/jsondumpCron.sh
+++ shell scripts/jsondumpCron.sh
@@ -1,4 +1,4 @@
 ms=$(date --date=$(date +"%D") +%s%N | cut -b1-13)
 path=/home/asro/Documents/$(date +%y-%m-%d)
-mongodump -d twitter -c count -q '{date: {$gte: new Date('"$ms"')}}' --out "$path"
-bsondump "$path"/twitter/count.bson > /var/www/html/"$(date +%Y-%m-%d)".json
+mongodump -h 134.58.106.9 --authenticationDatabase admin --username reader --password mAy1ReAd0 -d rate -c flow -q '{created_at: {$gte: new Date('"$ms"')}}' --out "$path"
+bsondump "$path"/rate/flow.bson > /var/www/html/count/"$(date +%Y-%m-%d)".json
